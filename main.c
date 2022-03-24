3ca67266ca5c9fa95be7ef56a1f0626c2e2fa625
per4 <per4@CompSci-06.local>
2022-03-24 12:15:13 -0700

    changes

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<ctype.h>
 
 #define WORD_LENGTH 6
 /* five characters and one null byte */
@@ -18,12 +19,14 @@ char *right_place_mask(char *guess, char *word) {
 }
 
 char *wrong_place_in_string_mask(char *guess, char *word) {
-    /* TODO: not let multiple letters map to the same letter */
     char *res = malloc(WORD_LENGTH);
-    int i, j;
+    int i, j, k;
     for(i=0; i<WORD_LENGTH; i++) {
         for (j=0; j<WORD_LENGTH; j++) {
-            if (guess[i] == word[j]) res[i] = guess[i];
+            /* check if the letter was previously mapped to */
+            for (k=0; k<WORD_LENGTH; k++)
+                if (res[k] == word[j]) break;
+            if (guess[i] == word[k]) res[i] = guess[i];
             else res[i] = '\0';
         }
     }
@@ -39,24 +42,40 @@ void display_word(char *word) {
 
 void results(char *word, char *guess) {
     char *right = right_place_mask(guess, word);
-    char *almost = wrong_place_in_string_mask(guess, word);
+    /* char *almost = wrong_place_in_string_mask(guess, word); */
     printf("the guess: %s\n", guess);
     printf("the word : %s\n", word);
+    /* printf("almost   : %s\n", almost); */
     printf("matching : ");
     display_word(right);
     puts(""); /* new line */
 }
 
-void is_all_alpha(char *str) {
+int is_all_alpha(char *str) {
+    /* if string is not null terminated, this will break
+       TODO: add size parameter */
     while (isalpha(*str)) str++;
-    if (*str == '\0') return true;
-    else return false;
+    if (*str == '\0') return 1;
+    else return 0;
+}
+
+int is_all_null(char *str, int size) {
+    for(;size>0; size--)
+        if (str[size-1] != '\0') return 0;
+    return 1;
 }
 
 int main() {
     char word[WORD_LENGTH] = "hello";
-    char guess[WORD_LENGTH];
-    char *is_null = NULL;
+    char guess[WORD_LENGTH] = "ooxxx";
+    char *mask = wrong_place_in_string_mask(guess, word);
+
+    display_word(mask);
+    puts("");
+
+    printf("is null: %d\n", is_all_null(mask, WORD_LENGTH));
+
+    /* char *is_null = NULL;
     while (1) {
         fputs("> ", stdout);
         is_null = fgets(guess, WORD_LENGTH, stdin);
@@ -64,7 +83,8 @@ int main() {
             break;
         getc(stdin);
         results(word, guess);
-    }
+    } */
+
 
     /* printf("> ");
     fgets(guess, WORD_LENGTH, stdin);
